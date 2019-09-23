c760c4cc6abf861c62b486677ea687d3390b4232
zygisau <zygimantas.augunas@mif.stud.vu.lt>
2019-09-23 21:52:50 +0300

    CHN: App to the OOP style.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
-#include "main.h"
-#include "functions.h"
+#include "./ZA256/ZA256.h"
 
 int main(int argc, char *argv[]) {
     if (argc == 2) { // read from arguments
-        cout << argv[1] << " - " << hex << encodeInput(argv[1]) << endl;
+        cout << argv[1] << " - " << hex << zygisau::ZA256::hash(argv[1]) << endl;
     } else {
         cout << "No arguments were passed or failed to parse them. Exiting..." << endl;
     }
